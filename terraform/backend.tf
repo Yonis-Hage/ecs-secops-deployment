94544e19b0bbe2fc13e0af79a7d578d0177a0bcc
Yonis Hage <crownpower721@gmail.com>
2025-06-26 22:34:55 +0100

    added dockerfile and tf changes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,13 +7,4 @@ terraform {
     encrypt        = true
   }
 }
-terraform {
-  required_version = ">= 1.3.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-  }
-}
